8b8c06936abf9679fae9cbe3f9998664fe6e367a
Chris Sprague <chris@chaoscontrol.org>
2022-06-09 21:15:18 -0400

    remove sb-admin

--- resources/views/admin/pages/settings.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.layout')
-
-@section('content')
-    @include('admin.partials.page_heading', ['page_title' => 'Settings'])
-    <x-test-component>
-        gsdfsdf sdf
-    </x-test-component>
-@endsection
