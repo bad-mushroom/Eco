c980dee9f531573210a53aa7a529f74ab695680f
Chris Sprague <chris@chaoscontrol.org>
2022-06-27 06:31:09 -0400

    Fix icon componenet

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,4 +1,7 @@
 @props([
-w
+    'icon'    => 'fas fa-circle-info',
+    'color'   => 'primary',
+    'size'    => '3',
+    'classes' => '',
 ])
 <i class="text-{{ $color }} {{ $icon }} fs-{{ $size }} {{ $classes }} me-2"></i>
